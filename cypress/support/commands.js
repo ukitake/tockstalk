d6a04818f820685ef9f7d50208d9465f325089d6
Jonathan Azoff <jon@postmodern.llc>
2024-02-25 15:09:20 -0800

    make slack webhook logging optional

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,15 @@
 const { IncomingWebhook } = require('@slack/webhook');
 const webhookUrl = Cypress.env('slackWebhookUrl');
-const webhook = new IncomingWebhook(webhookUrl);
+const webhook = webhookUrl ? new IncomingWebhook(webhookUrl) : null;
 
 Cypress.Commands.overwrite('log', (originalFn, ...msgs) => {
-	msgs.forEach(text => {
-		const payload = text.text ? text : { text }
-		payload.username = Cypress.env('slackUsername')
-		payload.icon_emoji = Cypress.env('slackIconEmoji')
-		webhook.send(payload)
-	})
+	if (webhook)
+		msgs.forEach(text => {
+			const payload = text.text ? text : { text }
+			payload.username = Cypress.env('slackUsername')
+			payload.icon_emoji = Cypress.env('slackIconEmoji')
+			webhook.send(payload)
+		})
 	return originalFn(...msgs);
 })
 	
